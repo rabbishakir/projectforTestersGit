b4de351d6c475310b4e66f9f0a58c0428129a2c6
rabbishakir <rabbishakir@gmail.com>
2023-07-23 12:30:41 -0400

    This is my first commit

---
+++ src/projectforTestersGit/TestClass.java
@@ -0,0 +1,13 @@
+package projectforTestersGit;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a project for git");
+		System.out.println("I will put it on stagiong for git");
+		
+
+	}
+
+}
